faba38a1b1cca4c422902f9bbf26a5e55148e8b6
Ayush Kumar Sinha <88913986+Ayushkr15@users.noreply.github.com>
2023-03-04 14:03:36 +0530

    updates to finest

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
+  faGithub,
   faFacebook,
   faTwitter,
   faInstagram,
@@ -14,67 +15,69 @@ import { Container, Row, Col } from "react-bootstrap";
 function Contact() {
   return (
     <div className="contact-us">
-    
       <Container>
-        <Col md={12} className="box">
-          <p className="title">
-            Feel free to reach out to us on any of our social media platforms
-          </p>
-          <div className="social-media">
-            <div className="social-details">
-              <a href="https://twitter.com/Ayushkr15_">
-                <FontAwesomeIcon icon={faTwitter} />
-              </a>
-              <span>@Ayushkr15_</span>
-            </div>
-            <div className="social-details">
-              <a href="https://github.com/Ayushkr15">
-                <FontAwesomeIcon icon={faTwitter} />
-              </a>
-              <span>Ayushkr15</span>
-            </div>
-            <div className="social-details">
-              <a href="https://www.linkedin.com/in/ayushkr13/">
-                <FontAwesomeIcon icon={faLinkedin} />
-              </a>{" "}
-              <span> Ayush Kumar Sinha</span>
-            </div>
+        <Row>
+          <Col xs={12} md={6} className="box">
+            <p className="title">
+              Feel free to reach out to us on any of our social media platforms
+            </p>
+            <div className="social-media">
+              <div className="social-details">
+                <a href="https://twitter.com/Ayushkr15_">
+                  <FontAwesomeIcon icon={faTwitter} />
+                </a>
+                <span>@Ayushkr15_</span>
+              </div>
+              <div className="social-details">
+                <a href="https://github.com/Ayushkr15">
+                  <FontAwesomeIcon icon={faGithub} />
+                </a>
+                <span>Ayushkr15</span>
+              </div>
+              <div className="social-details">
+                <a href="https://www.linkedin.com/in/ayushkr13/">
+                  <FontAwesomeIcon icon={faLinkedin} />
+                </a>{" "}
+                <span> Ayush Kumar Sinha</span>
+              </div>
 
-            <div className="social-details">
-              <a href="https://discord.com/">
-                <FontAwesomeIcon icon={faDiscord} />
-              </a>{" "}
-              <span> LazySenpai#3624</span>
+              <div className="social-details">
+                <a href="https://discord.com/">
+                  <FontAwesomeIcon icon={faDiscord} />
+                </a>{" "}
+                <span> LazySenpai#3624</span>
+              </div>
             </div>
-          </div>
-        
-          {/* <div className="social-media">
-            <div className="social-details">
-              <a href="https://twitter.com/HasChan123">
-                <FontAwesomeIcon icon={faTwitter} />
-              </a>
-              <span>@HasChan123</span>
-            </div>
-            <div className="social-details">
-              <a href="https://www.behance.net/hasnaamahmoud0MK">
-                <FontAwesomeIcon icon={faBehance} />
-              </a>
-              <span>Hasnaa Mahmoud</span>
-            </div>
-            <div className="social-details">
-              <a href="https://www.linkedin.com/in/hasnaa-mahmoud-5151b5244/">
-                <FontAwesomeIcon icon={faLinkedin} />
-              </a><span> Hasnaa Mahmoud</span>
-            </div>
+          </Col>
+          {/* <Col xs={12} md={6} className="box">
+            <div className="social-media">
+              <div className="social-details">
+                <a href="https://twitter.com/HasChan123">
+                  <FontAwesomeIcon icon={faTwitter} />
+                </a>
+                <span>@HasChan123</span>
+              </div>
+              <div className="social-details">
+                <a href="https://www.behance.net/hasnaamahmoud0MK">
+                  <FontAwesomeIcon icon={faBehance} />
+                </a>
+                <span>Hasnaa Mahmoud</span>
+              </div>
+              <div className="social-details">
+                <a href="https://www.linkedin.com/in/hasnaa-mahmoud-5151b5244/">
+                  <FontAwesomeIcon icon={faLinkedin} />
+                </a><span> Hasnaa Mahmoud</span>
+              </div>
 
-            <div className="social-details">
-              <a href="https://discord.com/">
-                <FontAwesomeIcon icon={faDiscord} />
-              </a>
-              <span> Hasnaa#4644</span>
+              <div className="social-details">
+                <a href="https://discord.com/">
+                  <FontAwesomeIcon icon={faDiscord} />
+                </a>
+                <span> Hasnaa#4644</span>
+              </div>
             </div>
-          </div> */}
-        </Col>
+          </Col> */}
+        </Row>
       </Container>
     </div>
   );
